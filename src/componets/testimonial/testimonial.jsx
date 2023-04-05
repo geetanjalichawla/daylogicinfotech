82b3343dca5fa72f290243e66cf7a53406b366cc
geetanjali chawla <geetc1919@gmail.com>
2023-04-05 16:04:30 +0530

    first commit

--- src/componets/testimonial/testimonial.jsx
+++ src/componets/testimonial/testimonial.jsx
@@ -30,13 +30,13 @@ const Testimonial = () => {
                 </div>
             </div>
             <div className="col-md-4">
-                <div className="card"> <i className="fa fa-quote-left u-color"></i>
+            <div className="card"> <i className="fa fa-quote-left u-color"></i>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
                     <div className="d-flex justify-content-between align-items-center">
-                        <div className="user-about"> <span className="font-weight-bold d-block">Mike Vincent</span> <span className="u-color">Designer | Developer</span>
-                            <div className="d-flex flex-row mt-1"> <i className="fa fa-star u-color"></i> <i className="fa fa-star u-color"></i> <i className="fa fa-star u-color"></i> <i className="fa fa-star u-color"></i> <i className="fa fa-star-o u-color"></i> </div>
+                        <div className="user-about"> <span className="font-weight-bold d-block">Sophia T.</span> <span className="u-color">Designer | Architect</span>
+                            <div className="d-flex flex-row mt-1"> <i className="fa fa-star u-color"></i> <i className="fa fa-star u-color"></i> <i className="fa fa-star u-color"></i> <i className="fa fa-star-o u-color"></i> <i className="fa fa-star-o u-color"></i> </div>
                         </div>
-                        <div className="user-image"> <img  src="https://i.imgur.com/At1IG6H.png" className="rounded-circle" width="70" alt =""/> </div>
+                        <div className="user-image"> <img src="https://i.imgur.com/o5uMfKo.jpg" className="rounded-circle" width="70" alt ="" /> </div>
                     </div>
                 </div>
             </div>
